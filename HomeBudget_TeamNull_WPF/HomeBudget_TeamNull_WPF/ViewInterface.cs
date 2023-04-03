a2c116366a816061951adc31359ed08496f70b67
William Cattrell <98350617+williamCattrell@users.noreply.github.com>
2023-04-03 11:48:14 -0400

    added interface mehtods in viewInterface

--- HomeBudget_TeamNull_WPF/HomeBudget_TeamNull_WPF/ViewInterface.cs
+++ HomeBudget_TeamNull_WPF/HomeBudget_TeamNull_WPF/ViewInterface.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Budget;
 
 namespace HomeBudget_TeamNull_WPF
 {
     interface ViewInterface
     {
+
+        public void DisplayAddedExpense(Expense expense);
+        public void DisplayAddedCategory(Category category);
+
+        public void DisplayError(string error);
+
+        public void DisplaySuccess(string message);
     }
 }
