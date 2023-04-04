eb4d40ba4b31eeea0d307d07583dde4d0aaa5956
Robert Cristian Neacsu <2032248@johnabbottcollege.net>
2023-04-04 11:45:11 -0400

    Merged main again

--- HomeBudget_TeamNull_WPF/HomeBudget_TeamNull_WPF/MainWindow.xaml.cs
+++ HomeBudget_TeamNull_WPF/HomeBudget_TeamNull_WPF/MainWindow.xaml.cs
@@ -1,9 +1,17 @@
 ﻿using Budget;
 using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
+using System.Windows.Forms;
+using MessageBox = System.Windows.MessageBox;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
+using SaveFileDialog = Microsoft.Win32.SaveFileDialog;
+using TextBox = System.Windows.Controls.TextBox;
+using TextBlock = System.Windows.Controls.TextBlock;
+using RadioButton = System.Windows.Controls.RadioButton;
+using MouseEventArgs = System.Windows.Input.MouseEventArgs;
+using System.IO;
+using System.Collections.Generic;
 
 namespace HomeBudget_TeamNull_WPF
 {
@@ -12,16 +20,17 @@ namespace HomeBudget_TeamNull_WPF
     /// </summary>
     public partial class MainWindow : Window, ViewInterface
     {
-        Presenter presenter;
+        private string fileName = "";
+        private string folderName = "";
 
+        Presenter presenter;
         public MainWindow()
         {
 
             InitializeComponent();
-            //presenter = new Presenter(this, "");
             ShowMenu();
             dp.SelectedDate = DateTime.Today;
-            catCB.ItemsSource = GetCategoryList();
+            
 
         }
 
@@ -46,6 +55,37 @@ namespace HomeBudget_TeamNull_WPF
         }
 
 
+        private void OpenExistingDb(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                if (folderName == "")
+                {
+                    dialog.InitialDirectory = "c:\\";
+                }
+                else
+                {
+                    dialog.InitialDirectory = folderName;
+                }
+                dialog.Filter = "Database File (*.db)|*.db";
+
+                if (dialog.ShowDialog() == true)
+                {
+                    fileName = dialog.FileName;
+                    MessageBox.Show("Existing DB file has been picked", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                    presenter = new Presenter(this, fileName);
+                    catCB.ItemsSource = GetCategoryList();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error");
+            }
+        }
+
+
         public void DisplayAddedExpense(DateTime date, int catId, double amount, string desc)
         {
             throw new NotImplementedException();
@@ -53,7 +93,10 @@ namespace HomeBudget_TeamNull_WPF
 
         public void DisplayAddedCategory(string desc, string type)
         {
-            throw new NotImplementedException();
+            string successMessage = $"Category successfully added.\n" +
+                $"Category Description: {desc}\n" +
+                $"Category Type: {type}";
+            MessageBox.Show(successMessage);
         }
 
         private void DescInput_GotMouseCapture(object sender, MouseEventArgs e)
@@ -148,6 +191,71 @@ namespace HomeBudget_TeamNull_WPF
             catTypeDisplay.Text = catDescDisplay.Text = string.Empty;
         }
 
+        private void AddCategory(object sender, RoutedEventArgs e)
+        {
 
+        }
+
+        private void OpenNewDb(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SaveFileDialog saveDialog = new SaveFileDialog();
+                if (folderName == "")
+                {
+                    saveDialog.InitialDirectory = "c:\\";
+                }
+                else
+                {
+                    saveDialog.InitialDirectory = folderName;
+                }
+                saveDialog.Filter = "Database File (*.db)|*.db";
+                saveDialog.FileName = "dbName";
+                saveDialog.DefaultExt = ".db";
+
+                if (saveDialog.ShowDialog() == true)
+                {
+                    fileName = saveDialog.FileName;
+                    try
+                    {
+                        File.WriteAllText(fileName, "");
+                        MessageBox.Show("New DB file has been created", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                        presenter = new Presenter(this, fileName);
+                        catCB.ItemsSource = GetCategoryList();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString(), "Error");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error");
+            }
+        }
+
+        private void OpenFolder(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+                folderDialog.InitialDirectory = "c:\\";
+                folderDialog.ShowNewFolderButton = true;
+                DialogResult result = folderDialog.ShowDialog();
+
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    folderName = folderDialog.SelectedPath;
+                    MessageBox.Show("DB folder has been chosen", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error");
+            }
+
+        }
     }
 }
