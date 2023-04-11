f788f082dadb471c7927f659436d5f0303c02285
2137571 <2137571@PE325-08>
2023-04-11 11:52:56 -0400

    added feature category dropdown no longer changes

--- HomeBudget_TeamNull_WPF/HomeBudget_TeamNull_WPF/MainWindow.xaml.cs
+++ HomeBudget_TeamNull_WPF/HomeBudget_TeamNull_WPF/MainWindow.xaml.cs
@@ -156,8 +156,7 @@ namespace HomeBudget_TeamNull_WPF
             bool success = double.TryParse(amountTB.Text, out amount);
             if (success)
             {
-                catCB.SelectedIndex = 0;
-                catCB.Text = string.Empty;
+                
                 amountTB.Clear();
                 descriptionTB.Clear();
 
