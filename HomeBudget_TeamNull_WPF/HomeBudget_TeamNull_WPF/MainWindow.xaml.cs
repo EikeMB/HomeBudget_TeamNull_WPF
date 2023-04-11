c823f6b9a9e41c5cea5b3a1fc64f7622bb378eb3
Robert Cristian Neacsu <2032248@johnabbottcollege.net>
2023-04-11 11:50:29 -0400

    Added an "Expense added" message

--- HomeBudget_TeamNull_WPF/HomeBudget_TeamNull_WPF/MainWindow.xaml.cs
+++ HomeBudget_TeamNull_WPF/HomeBudget_TeamNull_WPF/MainWindow.xaml.cs
@@ -163,6 +163,8 @@ namespace HomeBudget_TeamNull_WPF
 
                 presenter.processAddExpense(date, category, amount, description);
                 changeOccured = false;
+
+                MessageBox.Show("The expense has been succesfully added", "Added Expense", MessageBoxButton.OK);
             }
             else
             {
