cde48b00e88eff7b19f75c5dd25286e4ac2f6d1b
Robert Cristian Neacsu <2032248@johnabbottcollege.net>
2023-04-18 12:21:58 -0400

    Added GetCategoryList to Update Window

--- HomeBudget_TeamNull_WPF/HomeBudget_TeamNull_WPF/UpdateWindow.xaml.cs
+++ HomeBudget_TeamNull_WPF/HomeBudget_TeamNull_WPF/UpdateWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,12 +18,44 @@ namespace HomeBudget_TeamNull_WPF
     /// <summary>
     /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class Window1 : Window
+    public partial class Window1 : Window, ViewInterface
     {
+        private Presenter presenter;
+
+
         public Window1()
         {
             InitializeComponent();
             Update_DP.SelectedDate = DateTime.Now;
+            RefreshCategories(GetCategoryList());
+        }
+
+        public void DisplayAddedCategory(string desc, string type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisplayAddedExpense(DateTime date, string catId, double amount, string desc)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisplayError(string error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetCategoryList()
+        {
+            List<string> cats = new List<string>();
+            cats = presenter.GetCategoryDescriptionList();
+
+            return cats;
+        }
+        private void RefreshCategories(List<string> categoriesList)
+        {
+            update_CB.ItemsSource = categoriesList;
+            update_CB.Items.Refresh();
         }
     }
 }
